3f32d5a4e27d1d3d33aa1df146c598b045e57127
Praveen <chiliveri.praveen@philips.com>
2019-08-21 12:52:53 +0530

    code coverage

---
+++ MavenDemoProject/src/main/java/com/philips/Standardclass.java
@@ -0,0 +1,12 @@
+/*
+ * The copyright of this file belongs to Koninklijke Philips N.V., 2019.
+ */
+package com.philips;
+
+public class Standardclass {
+
+  public String sayHelloWorld() {
+    return "helloworld";
+  }
+
+}
---
+++ MavenDemoProject/src/test/java/com/test/AppTest.java
@@ -0,0 +1,17 @@
+/*
+ * The copyright of this file belongs to Koninklijke Philips N.V., 2019.
+ */
+package com.test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.philips.Standardclass;
+public class AppTest {
+  Standardclass app=new Standardclass();
+  @Test
+  public void Test() {
+    final String expected="helloworld";
+    assertEquals(expected,app.sayHelloWorld());
+  }
+
+}
