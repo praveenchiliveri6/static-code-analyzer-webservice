0a52168c20ee62621cf993bd8a099bc6e374be42
lalith maddala <lalithkumar1112@gmail.com>
2019-08-28 15:44:58 +0530

    updated classes

--- SpringBootSCA/src/main/java/com/philips/Controller.java
+++ SpringBootSCA/src/main/java/com/philips/Controller.java
@@ -6,24 +6,89 @@ package com.philips;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
   @Autowired
   Service service;
+  @Autowired
+  SourceInput si;
 
-  @GetMapping(value="/coverage")
+  String projectdir;
+  String projectname;
+
+  @PostMapping(path = "/start", consumes = "application/json")
+  public void compileandtest(@RequestBody SourceInput si) throws IOException, InterruptedException {
+    projectdir = si.getSourceCodeDirectory();
+    projectname = si.getProjectName();
+    System.out.println(projectdir+" "+projectname);
+    service.runcommand(Commands.mavenclean,Commands.mavenbinpath);
+    service.runcommand(Commands.mavencompile,Commands.mavenbinpath);
+    service.runcommand(Commands.maventestcompile,Commands.mavenbinpath);
+    service.runcommand(Commands.maveninstall,Commands.mavenbinpath);
+    if (!service.checkforrow(projectname)) {
+      service.insert(projectname);
+    }
+
+  }
+
+  @GetMapping(value = "/coverage")
   public void codecoverage() throws IOException, InterruptedException {
-    service.createfile(Commands.javaagentcommand);
-    Thread.sleep(5000);
-    service.createfile(Commands.javacommand);
-    final double codecoverage=service.parsefile("C:\\jacoco-executable\\report2.csv");
-    System.out.println("code coverage is: "+codecoverage*100);
+    service.runcommand(Commands.getjavaagent(projectdir),"");
+    service.runcommand(Commands.getjavacommand(projectdir),"");
+    final double cc = service.parseCsvFile("C:\\jacoco-executable\\report2.csv");
+    final Results result = service.getvalue(projectname);
+    final int codecoverage = (int) cc;
+    final int prevresult = result.getCodecoverage();
+    service.updatevalue(projectname, codecoverage);
+    if (codecoverage <= 80) {
+      System.out.println("no go");
+    } else {
+      System.out.println("go");
+      if (prevresult > codecoverage) {
+        System.out.println("codecoverage is worse than the previous");
+      }
+    }
   }
 
-  @GetMapping(value="/test")
+  @GetMapping(value = "/test")
   public void unittesting() throws IOException {
-    service.createandextract(Commands.testcommand);
+    service.createandextract(Commands.gettestcommand(projectdir));
   }
 
-}
+  @GetMapping(value = "/security")
+  public void showReport() throws IOException, InterruptedException {
+    service.runcommand(Commands.getsecuritycommand(projectdir, projectname),Commands.vcgPath);
+    service.parseTextFile(projectname);
+  }
+
+  @GetMapping("/duplicate")
+  public void showDuplicates() throws IOException, InterruptedException {
+    final int duplicate=service.runcommand(Commands.getduplicatecommand(projectdir),"");
+    if(duplicate==1) {
+      System.out.println("duplicates found");
+    } else {
+      System.out.println("No duplicates found");
+    }
+  }
+
+  @GetMapping(value="/complexity")
+  public int getComplexity() throws IOException{
+    {
+      service.runcommand(Commands.getcomplexitycommand(projectdir, projectname),Commands.cyvisBinPath);
+
+    }
+
+    @GetMapping(value="/pmd")
+    public void getPmdReport() throws IOException, InterruptedException
+    {
+      /*final String projectpath="C:\\Users\\320066613\\eclipse-workspace\\com.philips.simpleApp";
+      final String projectname=commandLineOperator.getProjectName(projectpath);*/
+      service.runcommand(Commands.getpmdcommand(projectdir, projectname),Commands.PmdBinPath);
+      service.parseXML(projectname);
+    }
+
+  }
--- SpringBootSCA/src/main/java/com/philips/Service.java
+++ SpringBootSCA/src/main/java/com/philips/Service.java
@@ -4,50 +4,147 @@
 package com.philips;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import com.opencsv.CSVReader;
 
 @org.springframework.stereotype.Service
 public class Service {
-  public void createfile(String command[]) throws IOException {
-    final ProcessBuilder pb1 = new ProcessBuilder(command);
-    pb1.start();
+  @Autowired
+  StaticToolDaoImpl staticToolDaoImpl;
+
+  public void insert(String projectname) {
+    staticToolDaoImpl.insert(projectname);
   }
+
+  public boolean checkforrow(String projectname) {
+    return staticToolDaoImpl.getallrows(projectname);
+  }
+
+  public Results getvalue(String projectname) {
+    return staticToolDaoImpl.get(projectname);
+  }
+
+  public void updatevalue(String projectname, int value) {
+    staticToolDaoImpl.update(projectname, value);
+  }
+
+  public int runcommand(String[] command, String binPath) {
+    final ProcessBuilder pb = new ProcessBuilder(command);
+    pb.directory(new File(Commands.projectdir));
+    final Map<String, String> envMap = pb.environment();
+    String path = envMap.get("Path");
+    path += binPath;
+    envMap.put("Path", path);
+    Process process;
+    try {
+      process = pb.start();
+    } catch (final IOException e) {
+      return 1;
+    }
+    try {
+      process.waitFor();
+    } catch (final InterruptedException e) {
+      return 1;
+    }
+    return process.exitValue();
+  }
+
+
 
   public void createandextract(String command[]) throws IOException {
     final ProcessBuilder pb1 = new ProcessBuilder(command);
     final Process process = pb1.start();
     final BufferedReader out = new BufferedReader(new InputStreamReader(process.getInputStream()));
     String s = null;
-    while ((s = out.readLine()) != null) {
-      if (s.length() != 0) {
-        System.out.println(s);
-      }
+    while ((s = out.readLine()) != null && s.charAt(0) != 'T') {
+      ;
+    }
+    if (s == null) {
+      System.out.println("0 ms");
+    } else {
+      final double time = Double.parseDouble((String) s.subSequence(6, s.length()));
+      System.out.println(time * 1000 + "ms");
     }
   }
 
-
-  public double parsefile(String file) throws IOException {
+  public double parseCsvFile(String file) throws IOException {
     final FileReader filereader = new FileReader(file);
     final CSVReader csvReader = new CSVReader(filereader);
+    double codecoverage = 0.0;
     try {
-      double codecoverage = 0.0;
       String[] nextRecord = csvReader.readNext();
       if ((nextRecord = csvReader.readNext()) != null) {
         final int instructionscovered = Integer.parseInt(nextRecord[4]);
         final int instructionsmissed = Integer.parseInt(nextRecord[3]);
         codecoverage = (double) instructionscovered / (instructionscovered + instructionsmissed);
       }
-      return codecoverage;
     } catch (final Exception e) {
       e.printStackTrace();
     } finally {
       filereader.close();
       csvReader.close();
     }
-    return 0.0;
+    return codecoverage * 100;
   }
 
+  public int parseTextFile(String str) throws IOException {
+    String line;
+    int count = 0;
+    final FileReader fr =
+        new FileReader("C:\\Users\\320065909\\Desktop\\security\\" + str + ".txt");
+    @SuppressWarnings("resource")
+    final BufferedReader br = new BufferedReader(fr);
+    while ((line = br.readLine()) != null) {
+      final String[] words = line.split(" ");
+      if (words[0].equals("Line:")) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public int parseXML(String projectname) {
+
+    try {
+      int noofissues = 0;
+      final File fXmlFile = new File("C:\\Users\\" + projectname + ".xml");
+      if (fXmlFile.exists()) {
+        final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        final Document doc = dBuilder.parse(fXmlFile);
+        final NodeList nList = doc.getElementsByTagName("file");
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+          final Node nNode = nList.item(temp);
+          if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+            final Element eElement = (Element) nNode;
+            final NodeList violations = eElement.getElementsByTagName("violation");
+            noofissues += violations.getLength();
+          }
+        }
+      } else {
+        System.out.println(" file is not created");
+        System.out.println("The possible reasons are :\n");
+        System.out.println("The user didn't install pmd in C: Drive.\n");
+        System.out.println("The user may have installed the different version of pmd"
+            + " other than we provided in README file.\n");
+      }
+      return noofissues;
+    } catch (final Exception e) {
+      return -1;
+
+    }
+  }
 }
+
+
